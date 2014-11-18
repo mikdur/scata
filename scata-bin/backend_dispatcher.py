2c02985bd11044422fa2043eee66b4ec5e3401b1
Mikael Brandström Durling <mikael.durling@slu.se>
2014-11-18 09:14:50 +0100

    Increase memory allocation for main tasks.

--- scata-bin/backend_dispatcher.py
+++ scata-bin/backend_dispatcher.py
@@ -119,7 +119,7 @@ while True:
     n = 0
     for row in db_c:
         sge_wrapper.run(base_dir + "/start_scata.py", [ str(row["idJobs"])],
-                        "ScataJ%d" % (row["idJobs"]), sge_params_scata + str(row["sgeProject"]), 60 * 24 * 20, "10G")
+                        "ScataJ%d" % (row["idJobs"]), sge_params_scata + str(row["sgeProject"]), 60 * 24 * 20, "15G")
         db_c.execute("UPDATE Jobs SET locked=1 WHERE idJobs = %s", (row["idJobs"],))
         db.commit()
         n += 1
