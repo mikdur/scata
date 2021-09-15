d8190fa1dfc3aeba5bd813f4711d2fee20a86e32
Mikael Brandström Durling <mikael.durling@slu.se>
2021-09-15 14:18:51 +0200

    Minor cleanup.

--- scata-bin/scatad.sh
+++ scata-bin/scatad.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-. /opt/sw/sge/8.1.6/default/common/settings.sh
+source /opt/sw/sge/8.1.9/default/common/settings.sh
+source /scata/scata-system/scata-env/bin/activate
 
 cd /scata/scata-run/tmp
 while true
   do
-  echo hej
+  echo "Trying to start ($(date))"
   python /scata/scata-system/scata-bin/backend_dispatcher.py >> ../log/bd.log 2>&1
   sleep 10
   done
